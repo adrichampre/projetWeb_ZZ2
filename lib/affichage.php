7671b236440f21c0c9fcc8bc4acfb1a2edcb663b
Adrien Champredonde <adrien.champredonde@etu.udamail.fr>
2020-03-16 18:13:46 +0100

    Correction affichage pour mobile

--- lib/affichage.php
+++ lib/affichage.php
@@ -15,67 +15,42 @@ function affichage_familles($db){
     $result = $db->query($sql) or die('Erreur SQL : '.mysqli_error($db));
     While($data = mysqli_fetch_array($result))
     {
-        echo '<a href="index.php?Famille=' . $data['id'] . '">';
-        echo '<figure>';
-        echo '<img src="img_familles/'. $data['image'] . '" alt=""/>';
-        echo '<figcaption>'.$data['libelle'] .'</figcaption>';
-        echo '</figure>';
-        echo '</a>';
+        echo '<a href="index.php?Famille=' . $data['id'] . '">
+                 <figure>
+                    <img src="img_familles/'. $data['image'] . '" alt=""/>
+                    <figcaption>'.$data['libelle'] .'</figcaption>
+                 </figure>
+              </a>';
     }
 }
-/*
-function affichage_articles($id_famille, $db){
-    echo '<a class="myButton" href="index.php" style="float: left">Retour</a>';
-    $sql = 'SELECT reference, libelle, detail, prix_ttc, id_tva, image FROM article WHERE id_famille ='. $id_famille;
-    $result = $db->query($sql) or die('Erreur SQL : '.mysqli_error($db));
-    //Utiliser des tables pour afficher infos
-    While($data = mysqli_fetch_array($result))
-    {
-        echo '<div id="article">';
-            echo '<img src="img_articles/'. $data['image'] . '" alt=""/>';
-            echo '<div id="contenuArticle">';
-                echo '<h3>'.$data['libelle'].'</h3>';
-                echo '<h4>'.$data['detail'] .'</h4>';
-                echo '<table><tr><td>'.$data['prix_ttc'].'€</td> <td> <a class="myButton">Commander</a></tr></table>';
-            echo '</div>';
-        echo '</div>';
-    }
-}*/
 
 function affichage_articles($id_famille, $db){
-    $a = 0;
     echo '<a class="myButton" href="index.php" style="float: left">Retour</a>';
     $sql = 'SELECT reference, libelle, detail, prix_ttc, id_tva, image FROM article WHERE id_famille ='. $id_famille;
     $result = $db->query($sql) or die('Erreur SQL : '.mysqli_error($db));
     //Utiliser des tables pour afficher infos
-    echo '<table id="tableArticles">';
+    echo '<div id="divArticles">';
     While($data = mysqli_fetch_array($result))
     {
-        if(($a % 2) == 0){
-            echo '<tr>';
-        }
-        echo '<td>
-                <table id="article">
+        echo '<table id="article">
                     <tr>
                         <td rowspan="2">
                             <img src="img_articles/'. $data['image'] . '" alt=""/>
                         </td>
                         <td colspan="2">
-                            <p id="libelle">'.$data['libelle'].$a.'</p>
+                            <p id="libelle">'.$data['libelle'].'</p>
                             <p id="detail">'.$data['detail'] .'</p>
                         </td>
                     </tr>
                     <tr>
-                        <td>'.$data['prix_ttc'].'€</td> 
+                        <td id="prix">'.$data['prix_ttc'].'€</td> 
                         <td> 
                             <a class="myButton">Commander</a>
                         </td>
                     </tr>
-                </table>
-               </td>';
-        $a = $a + 1;
+               </table>';
     }
-    echo '</table>';
+    echo '</div>';
 }
 
 
